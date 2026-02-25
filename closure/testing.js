a7de48d1b44abe178becdac24a50d5bf1fbee63e
neetesh <neeteshlodhi9555@email.com>
2026-02-25 20:31:42 +0530

    Test Javascript function

--- closure/testing.js
+++ closure/testing.js
@@ -19,3 +19,27 @@ let fun = person()
 let fun2 = fun()
 console.log(fun2) //that return the showAge
 fun2()
+
+
+
+// function person() {
+//           let name = "Lodhi"
+
+//           function showName() {
+//                     console.log(name)
+//                     let age = 20
+
+//                     function showAge() {
+//                               console.log(age)
+//                               console.log(name)
+//                     }
+//                     return showAge;
+//           }
+//           return showName;
+// }
+
+// let fun = person()
+// // console.log(fun)
+// let fun2 = fun()
+// console.log(fun2) //that return the showAge
+// fun2()
